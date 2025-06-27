b118bd4cf2f3c1a93b1a34a85ee164a1a38754e3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-06-27 09:41:08 +0200

    Add headers

--- FastDraw2D/ViewModels/MainWindowViewModel.cs
+++ FastDraw2D/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace FastDraw2D.ViewModels;
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+namespace FastDraw2D.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
